80b9bd0378aac0245608c252069a50d1824645b6
Raik Bitters <raikbitters@gmail.com>
2022-03-28 00:56:59 +0300

    Add map struct and map constructor

--- src/map.rs
+++ src/map.rs
@@ -2,3 +2,20 @@ use crate::prelude::*;
 
 const NUM_TILES: usize = (SCREEN_WIDTH * SCREEN_HEIGHT) as usize;
 
+#[derive(Clone, Copy, PartialEq)]
+pub enum TileType {
+    Wall,
+    Floor,
+}
+
+pub struct Map {
+    pub tiles: Vec<TileType>,
+}
+
+impl Map {
+    pub fn new() -> Self {
+        Self {
+            tiles: vec![TileType::Floor; NUM_TILES],
+        } 
+    }
+}
