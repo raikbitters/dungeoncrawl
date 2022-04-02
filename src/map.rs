9fcde721f8476f011beea25bd13bd06fda71b705
Raik Bitters <raikbitters@gmail.com>
2022-04-02 17:13:22 +0300

    Add map render

--- src/map.rs
+++ src/map.rs
@@ -8,6 +8,10 @@ pub enum TileType {
     Floor,
 }
 
+pub fn map_idx(x: i32, y:i32) -> usize {
+    ((y * SCREEN_WIDTH) + x) as usize
+}
+
 pub struct Map {
     pub tiles: Vec<TileType>,
 }
@@ -17,5 +21,21 @@ impl Map {
         Self {
             tiles: vec![TileType::Floor; NUM_TILES],
         } 
+    }
+
+    pub fn render(&self, ctx: &mut BTerm) {
+        for y in 0..SCREEN_HEIGHT {
+            for x in 0..SCREEN_WIDTH {
+                let idx = map_idx(x, y);
+                match self.tiles[idx] {
+                    TileType::Floor => {
+                        ctx.set(x, y, YELLOW, BLACK, to_cp437('.'));
+                    }
+                    TileType::Wall => {
+                        ctx.set(x, y, GREEN, BLACK, to_cp437('#'));
+                    }
+                }
+            }
+        }
     }
 }
