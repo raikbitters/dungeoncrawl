7523f900ac97e2b4d6ac02d204bb2a5ea91c2f29
raikbitters <raikbitters@gmail.com>
2023-01-01 18:05:22 +0400

    Add map_render_system

--- src/systems/mod.rs
+++ src/systems/mod.rs
@@ -6,5 +6,6 @@ mod map_render;
 pub fn build_scheduler() -> Schedule {
     Schedule::builder()
         .add_system(player_input::player_input_system())
+        .add_system(map_render::map_render_system())
         .build()
 }
